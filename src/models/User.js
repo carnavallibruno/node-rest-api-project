6d4b113167d34f2e1c201fee952947c89a41ff73
Bruno Carnavalli <carnavallibruno@gmail.com>
2022-09-07 18:32:35 -0300

    Added validations to the fields.

--- src/models/User.js
+++ src/models/User.js
@@ -2,19 +2,22 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    id: {type: String},
-    name: {type: String, required: true},
-    cpf: {type: String, minlength: 11, maxlength: 11},
-    birthDate: {type: Date},
-    email: {type: String, validate: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, required: true},
-    password: {type: String, minlength: 6},
-    address: {type: String},
-    number: {type: String},
-    complement: {type: String},
-    city: {type: String},
-    state: {type: String},
-    country: {type: String},
-    zipCode: {type: String}
+    id: { type: String },
+    name: { type: String, validate: /^[A-Za-z\s]+$/, required: true },
+    cpf: { type: String, validate: /^[0-9]*$/, minlength: 11, maxlength: 11, required: true },
+    birthDate: { type: Date, required: true },
+    email: { type: String, validate: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/, required: true },
+    password: { type: String, minlength: 6, required: true },
+    address: { type: String, required: true },
+    number: { type: String, validate: /^[0-9]*$/, required: true },
+    complement: { type: String, required: true },
+    city: { type: String, validate: /^[A-Za-z\s]+$/, required: true },
+    state: { type: String, validate: /^[A-Za-z\s]+$/, required: true },
+    country: { type: String, validate: /^[A-Za-z\s]+$/, required: true },
+    zipCode: { type: String, validate: /^[0-9]*$/, minlength: 8, maxlength: 8, required: true }
+  },
+  {
+    versionKey: false
   }
 )
 
