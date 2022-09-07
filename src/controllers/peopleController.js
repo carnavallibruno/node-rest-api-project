f9ea1f5d06ed3228a1948ad8912710e212e2ba9d
Bruno Carnavalli <carnavallibruno@gmail.com>
2022-09-06 23:02:54 -0300

    Set both post and put methods inside the controller.

--- src/controllers/peopleController.js
+++ src/controllers/peopleController.js
@@ -23,6 +23,30 @@ class PersonController {
       }
     })
   }
+
+  static postPerson = (req, res) => {
+    let person = new people(req.body);
+
+    person.save((err) => {
+      if (err) {
+        res.status(500).send({message: `${err.message} - Falha ao cadastrar pessoa`})
+      } else {
+        res.status(201).send(person.toJSON())
+      }
+    })
+  }
+
+  static putPerson = (req, res) => {
+    const personId = req.params.id;
+
+    people.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+      if (err) {
+        res.status(500).send({message: `${err.message} - falha ao atualizar a informação do usuário`})
+      } else {
+        res.status(200).send({message: `Informação do usuário atualizada com sucesso`})
+      }
+    })
+  }
 }
 
 export default PersonController;
